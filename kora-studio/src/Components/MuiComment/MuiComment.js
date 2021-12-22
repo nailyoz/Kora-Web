41d26278f323ea4762af9ae644e258cd7c300b7f
Faitly <fty.57@alu.ufc.br>
2021-12-22 10:57:44 -0300

    Algum estilo no MuiComment

--- kora-studio/src/Components/MuiComment/MuiComment.js
+++ kora-studio/src/Components/MuiComment/MuiComment.js
@@ -1,78 +1,85 @@
+// IMPORTS
 import React from 'react'
-import 'semantic-ui-css/semantic.min.css'
 import { Button, Comment, Form, Header } from 'semantic-ui-react'
+
+// CSS
+import './styles.css'
+import 'semantic-ui-css/semantic.min.css'
 
 
 const MuiComment = () => (
-  <Comment.Group>
-    <Header as='h3' dividing>
-      Comments
-    </Header>
+     <Comment.Group size='large'>
+          <Header as='h3' dividing>
+               Aprimore o passo
+          </Header>
 
-    <Comment>
-      <Comment.Avatar src='https://react.semantic-ui.com/images/avatar/small/matt.jpg' />
-      <Comment.Content>
-        <Comment.Author as='a'>Matt</Comment.Author>
-        <Comment.Metadata>
-          <div>Today at 5:42PM</div>
-        </Comment.Metadata>
-        <Comment.Text>How artistic!</Comment.Text>
-        <Comment.Actions>
-          <Comment.Action>Reply</Comment.Action>
-        </Comment.Actions>
-      </Comment.Content>
-    </Comment>
+          <Comment className='card-comment'>
+               <Comment.Avatar src='https://react.semantic-ui.com/images/avatar/small/matt.jpg' />
+               <Comment.Content>
+                    <Comment.Author as='a'>Álisson</Comment.Author>
+                    <Comment.Metadata>
+                         <div>Hoje às 5:42PM</div>
+                    </Comment.Metadata>
+                    <Comment.Text>Ficou incrível!</Comment.Text>
+                    <Comment.Actions>
+                         <Comment.Action>Responder</Comment.Action>
+                    </Comment.Actions>
+               </Comment.Content>
+          </Comment>
 
-    <Comment>
-      <Comment.Avatar src='https://react.semantic-ui.com/images/avatar/small/elliot.jpg' />
-      <Comment.Content>
-        <Comment.Author as='a'>Elliot Fu</Comment.Author>
-        <Comment.Metadata>
-          <div>Yesterday at 12:30AM</div>
-        </Comment.Metadata>
-        <Comment.Text>
-          <p>This has been very useful for my research. Thanks as well!</p>
-        </Comment.Text>
-        <Comment.Actions>
-          <Comment.Action>Reply</Comment.Action>
-        </Comment.Actions>
-      </Comment.Content>
-      <Comment.Group>
-        <Comment>
-          <Comment.Avatar src='https://react.semantic-ui.com/images/avatar/small/jenny.jpg' />
-          <Comment.Content>
-            <Comment.Author as='a'>Jenny Hess</Comment.Author>
-            <Comment.Metadata>
-              <div>Just now</div>
-            </Comment.Metadata>
-            <Comment.Text>Elliot you are always so right :)</Comment.Text>
-            <Comment.Actions>
-              <Comment.Action>Reply</Comment.Action>
-            </Comment.Actions>
-          </Comment.Content>
-        </Comment>
-      </Comment.Group>
-    </Comment>
+          <Comment>
+               <Comment className='card-comment'>
+                    <Comment.Avatar src='https://react.semantic-ui.com/images/avatar/small/elliot.jpg' />
+                    <Comment.Content>
+                         <Comment.Author as='a'>Matheus</Comment.Author>
+                         <Comment.Metadata>
+                              <div>Ontem às 12:30AM</div>
+                         </Comment.Metadata>
+                         <Comment.Text>
+                              <p>Incrível Ana! Fico feliz que tenha gostado!</p>
+                         </Comment.Text>
+                         <Comment.Actions>
+                              <Comment.Action>Responder</Comment.Action>
+                         </Comment.Actions>
+                    </Comment.Content>
+               </Comment>
 
-    <Comment>
-      <Comment.Avatar src='https://react.semantic-ui.com/images/avatar/small/joe.jpg' />
-      <Comment.Content>
-        <Comment.Author as='a'>Joe Henderson</Comment.Author>
-        <Comment.Metadata>
-          <div>5 days ago</div>
-        </Comment.Metadata>
-        <Comment.Text>Dude, this is awesome. Thanks so much</Comment.Text>
-        <Comment.Actions>
-          <Comment.Action>Reply</Comment.Action>
-        </Comment.Actions>
-      </Comment.Content>
-    </Comment>
+               <Comment.Group>
+                    <Comment className='card-comment'>
+                         <Comment.Avatar src='https://react.semantic-ui.com/images/avatar/small/jenny.jpg' />
+                         <Comment.Content>
+                              <Comment.Author as='a'>Naily</Comment.Author>
+                              <Comment.Metadata>
+                                   <div>Agora mesmo</div>
+                              </Comment.Metadata>
+                              <Comment.Text>Você põe bastante força no movimento peitoral do segundo passo, na verdade o passo é uma ondulação nas costas, assim você não perderia a energia para o terceiro movimento. Obrigada!</Comment.Text>
+                              <Comment.Actions>
+                                   <Comment.Action>Responder</Comment.Action>
+                              </Comment.Actions>
+                         </Comment.Content>
+                    </Comment>
 
-    <Form reply>
-      <Form.TextArea />
-      <Button content='Add Reply' labelPosition='left' icon='edit' primary />
-    </Form>
-  </Comment.Group>
+                    <Comment className='card-comment'>
+                         <Comment.Avatar src='https://react.semantic-ui.com/images/avatar/small/joe.jpg' />
+                         <Comment.Content>
+                              <Comment.Author as='a'>Lucas Holanda</Comment.Author>
+                              <Comment.Metadata>
+                                   <div>Agora mesmo</div>
+                              </Comment.Metadata>
+                              <Comment.Text>Também estou treinando essa dança, acho um passo bem complexo!</Comment.Text>
+                              <Comment.Actions>
+                                   <Comment.Action>Responder</Comment.Action>
+                              </Comment.Actions>
+                         </Comment.Content>
+                    </Comment>
+               </Comment.Group>
+          </Comment>
+
+          <Form reply>
+               <Form.TextArea />
+               <Button content='Adicionar Comentário' labelPosition='left' icon='edit' primary />
+          </Form>
+     </Comment.Group>
 )
 
 export default MuiComment
